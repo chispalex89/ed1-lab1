5fe3e6b45b181c5272c0fb78984d408d0405e6a5
chispalex89 <chispalex89@gmail.com>
2022-01-27 07:29:22 -0600

    Add Pokemon model

---
+++ Laboratorio 1/Models/PokemonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class PokemonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+    }
+}
