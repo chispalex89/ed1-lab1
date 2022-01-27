681947a7e79fb537e35d8ced271eb6d48b937947
chispalex89 <chispalex89@gmail.com>
2022-01-27 07:58:36 -0600

    Added controllers

---
+++ Laboratorio 1/Controllers/PokemonController.cs
@@ -0,0 +1,101 @@
+﻿using Laboratorio_1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Laboratorio_1.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: PokemonController
+        public ActionResult Index()
+        {
+            var list = new List<PokemonModel>
+            {
+                new PokemonModel
+                {
+                    Id = 7,
+                    Name = "Squirtle",
+                    HP = 44,
+                    Attack = 48,
+                    Defense = 65,
+                    SpecialAttack = 50,
+                    SpecialDefense = 64,
+                    Speed = 43
+                }
+            };
+            return View(list);
+        }
+
+        // GET: PokemonController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            //var model = list[id];
+            return View();
+        }
+
+        // GET: PokemonController/Create
+        public ActionResult Create()
+        {
+            return View(new PokemonModel());
+        }
+
+        // POST: PokemonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PokemonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PokemonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PokemonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PokemonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Laboratorio 1/Models/PokemonModel.cs
+++ Laboratorio 1/Models/PokemonModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Laboratorio_1.Models
+﻿namespace Laboratorio_1.Models
 {
     public class PokemonModel
     {
