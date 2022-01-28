1199ddbe000a61f724897bd51dcf068a447d0cf1
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-01-28 01:35:40 -0600

    Add FNAF Model

---
+++ Laboratorio 1/Models/FnafModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class FnafModel
+    {
+
+        public string Game { get; set; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Speed { get; set; }
+        public int Frecuency { get; set; }
+    }
+}
