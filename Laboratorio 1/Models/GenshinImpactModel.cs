84d7827360351cddbe547842efdf462fb7d6517b
Jose Mario Marroquin <jmmr114@gmail.com>
2022-01-30 15:04:02 -0600

    Add Genshin Impact model

---
+++ Laboratorio 1/Models/GenshinImpactModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class GenshinImpactModel
+    {
+        public string CharacterName { get; set; }
+
+        public int Rarity { get; set; }
+
+        public string Element { get; set; }
+
+        public string Weapon { get; set; }
+
+        public string Nation { get; set; }
+
+        public int BaseAttack { get; set; }
+
+        public int BaseDefense { get; set; }
+
+        public int BaseHP { get; set; }
+    }
+}
