9e15b2cbb20396300a2ac24744a55effb7733fc1
SARUCA1 <santiagoruizc@gmail.com>
2022-02-01 15:14:06 -0600

    Add NBA model

---
+++ Laboratorio 1/Models/NBAModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class NBAModel
+    {
+        public string Equipo { get; set; }
+        public string Base { get; set; }
+        public string Escolta { get; set; }
+        public string Alero { get; set; }
+        public string AlaPivot { get; set; }
+        public string Pivot { get; set; }
+    }
+}
