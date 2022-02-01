c5800331e06aaaa11077480d2459d01b78bf6501
danieelfcr <danielfcab2002@gmail.com>
2022-02-01 14:29:17 -0600

    Add Planets model

---
+++ Laboratorio 1/Models/PlanetsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class PlanetsModel
+    {
+        public int Name { get; set; }
+        public int Distance_From_Sun { get; set; }
+        public int Radius { get; set; }
+        public int Volume { get; set; }
+        public int Mass { get; set; }
+        public int Temperature { get; set; }
+        public int Gravity { get; set; }
+        public int Density { get; set; }
+    }
+}
