bdd3ae05f04deba6a37ec2ea74c91df2c0e2da77
VictorBautista <vm10bautista@gmail.com>
2022-02-01 17:06:20 -0600

    Agregué Persona5 model

---
+++ Laboratorio 1/Models/Persona5Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class Persona5Model
+    {
+        public int Id { get; set; }
+
+        public int HP { get; set; }
+
+        public int SPOrMP { get; set; }
+
+        public int Strenght { get; set; }
+
+        public int Inteligence { get; set; }
+
+        public int Magic { get; set; }
+
+        public int EnduranceOrVitality { get; set; }
+
+        public int DexterityOrTechnique { get; set; }
+
+        public int Agility { get; set; }
+
+        public int Luck { get; set; }
+    }
+}
