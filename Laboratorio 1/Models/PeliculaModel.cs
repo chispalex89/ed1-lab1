3b41146fd3652872808a1d7ecabb1b64db5c56f2
DiegoBautista <da10bautista@gmail.com>
2022-02-01 18:45:34 -0600

    Agregué modelo de Películas

---
+++ Laboratorio 1/Models/PeliculaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class PeliculaModel
+    {
+        public string Nombre { get; set; }
+        public int AñoPublicacion { get; set; }
+        public double Duracion { get; set; }
+        public string Genero { get; set; }
+        public string Director { get; set; }
+        public List<string> Actores { get; set; }
+        public bool Disponibilidad { get; set; }
+    }
+}
