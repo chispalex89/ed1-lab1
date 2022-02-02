cadcee86396c67cd3b1d20431aac9e2d667a1a91
Edryan López <you@example.com>
2022-02-02 12:37:05 -0600

    Add Dog model

---
+++ Laboratorio 1/Models/Dogmodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class Dogmodel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int attack { get; set; }
+        public int defense { get; set; }
+        public int specialattack { get; set; }
+        public int specialdeffense { get; set; }
+        public int speed { get; set; }
+        public int HP { get; set; }
+    }
+}
