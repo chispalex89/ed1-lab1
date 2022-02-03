bda16ba23562b095c727fb680174d8603d6b2c1a
Megan-Morales <meganm.beta@gmail.com>
2022-02-02 18:33:36 -0600

    Add Animal Crossing Model

---
+++ Laboratorio 1/Models/AnimalCrossingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Models
+{
+    public class AnimalCrossingModel
+    {
+        public int Name { get; set; }
+        public int Fruit { get; set; }
+        public int Insects { get; set; }
+        public int Animals { get; set; }
+        public int Tricks { get; set; }
+        public int Emotions { get; set; }
+
+
+    }
+}
