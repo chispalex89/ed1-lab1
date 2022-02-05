845df9df5ab088249f8922080e300fe5036bf500
Jose Mario Marroquin <jmmr114@gmail.com>
2022-02-04 22:09:23 -0600

    New views and genshin impact controller added

---
+++ Laboratorio 1/Helpers/GenshinImpactData.cs
@@ -0,0 +1,41 @@
+﻿using Laboratorio_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1.Helpers
+{
+    public class GenshinImpactData
+    {
+        private static GenshinImpactData _instance = null;
+
+        public static GenshinImpactData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GenshinImpactData();
+                }
+                return _instance;
+            }
+        }
+
+        public List<GenshinImpactModel> genshinList = new List<GenshinImpactModel>
+        {
+            new GenshinImpactModel
+            {
+                Id = 1,
+                CharacterName = "Arataki Itto",
+                Rarity = 5,
+                Element = "Geo",
+                Weapon = "Claymore",
+                Nation = "Inazuma",
+                BaseAttack = 300,
+                BaseDefense = 500,
+                BaseHP = 1500
+            }
+        };
+    }
+}
--- Laboratorio 1/Models/GenshinImpactModel.cs
+++ Laboratorio 1/Models/GenshinImpactModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Laboratorio_1.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,20 +9,70 @@ namespace Laboratorio_1.Models
 {
     public class GenshinImpactModel
     {
+        
+
+        [Required]
+        [Range(1, 20)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string CharacterName { get; set; }
 
+        [Required]
+        [Range(4,5)]
         public int Rarity { get; set; }
-
+        
+        [Required]
+        [MaxLength(10)]
         public string Element { get; set; }
 
+        [Required]
+        [MaxLength(10)]
         public string Weapon { get; set; }
 
+        [Required]
+        [MaxLength(10)]
         public string Nation { get; set; }
 
+        [Required]
+        [Range(300,500)]
         public int BaseAttack { get; set; }
 
+        [Required]
+        [Range(300, 500)]
         public int BaseDefense { get; set; }
 
+        [Required]
+        [Range(1000, 3000)]
         public int BaseHP { get; set; }
+
+
+
+        public static bool Save(GenshinImpactModel model)
+        {
+            GenshinImpactData.Instance.genshinList.Add(model);
+            return true;
+        }
+
+        public static void Edit(GenshinImpactModel model, int id)
+        {
+            GenshinImpactData.Instance.genshinList[id-1].CharacterName = model.CharacterName;
+            GenshinImpactData.Instance.genshinList[id-1].Rarity = model.Rarity;
+            GenshinImpactData.Instance.genshinList[id-1].Element = model.Element;
+            GenshinImpactData.Instance.genshinList[id-1].Weapon = model.Weapon;
+            GenshinImpactData.Instance.genshinList[id-1].Nation = model.Nation;
+            GenshinImpactData.Instance.genshinList[id-1].BaseAttack = model.BaseAttack;
+            GenshinImpactData.Instance.genshinList[id-1].BaseDefense = model.BaseDefense;
+            GenshinImpactData.Instance.genshinList[id-1].BaseHP = model.BaseHP;
+            
+        }
+
+        public static void Delete(GenshinImpactModel model)
+        {
+            GenshinImpactData.Instance.genshinList.Remove(model);
+        }
+      
+
     }
 }
